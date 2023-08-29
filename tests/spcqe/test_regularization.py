d44b4f12809b4121a42926349a3bac3dc9416121
Meyers-Im <bennetm@stanford.edu>
2023-08-29 14:15:12 -0700

    fixing test class name

--- tests/spcqe/test_regularization.py
+++ tests/spcqe/test_regularization.py
@@ -4,7 +4,7 @@ from spcqe.functions import make_regularization_matrix
 from spcqe.functions_old import regularization_matrix_3, regularization_matrix_2
 
 
-class TestBasis(unittest.TestCase):
+class TestRegularization(unittest.TestCase):
     def test_reg_3(self):
         reg_a = make_regularization_matrix(3, 1, [11, 17, 23])
         reg_b = regularization_matrix_3(3, 1, 11, 17, 23)
