4c3be7e15c0479990e367ba72cd7709f2cb35e54
Aramis <aramisduf@gmail.com>
2024-02-25 15:06:10 -0800

    docstring and formatting

--- src/spcqe/functions.py
+++ src/spcqe/functions.py
@@ -1,19 +1,58 @@
+"""
+This module contains functions to create basis and
+regularization matrices for a time series with one or
+multiple periods.
+"""
+
 import numpy as np
 from scipy.sparse import spdiags
 from itertools import combinations
 
 
-def make_basis_matrix(num_harmonics, length, periods, standing_wave=False, trend=False, max_cross_k=None, custom_basis=None):
+def make_basis_matrix(
+    num_harmonics,
+    length,
+    periods,
+    standing_wave=False,
+    trend=False,
+    max_cross_k=None,
+    custom_basis=None,
+):
+    """
+    This function creates a basis matrix for a time series with one or multiple
+    periods. It creates a Fourier basis matrix for each period, then combines
+    them into a single matrix, including cross-terms. It also includes an offset
+    term, and can include a linear trend.
 
-    sort_idx, Ps, num_harmonics, standing_wave = initialize_arrays(num_harmonics, periods, standing_wave, custom_basis)
-
+    :param num_harmonics: int or 1darray of ints, number of harmonics to use for
+        each period.
+    :param length: int, length of the time series.
+    :param periods: float or 1darray of floats, periods of signal. Each period will
+        yield a Fourier basis matrix.
+    :param standing_wave: boolean or 1darray of booleans, whether to use standing
+        wave basis for each period. A standing wave basis only includes sine terms
+        and starts at T/2. Defaults to False.
+    :param trend: boolean, whether to include a linear trend in the basis matrix.
+        Defaults to False.
+    :param max_cross_k: int, maximum number of cross terms. Defaults to None.
+    :param custom_basis: dict, allows the user to pass a dictionary of custom basis
+        matrices. The key is the index of the period and the value is the basis
+        matrix. The user can input a shorter basis than length, but the associated
+        period must be an int. Defaults to None.
+    :return: 2darray, basis matrix. The shape depends on the parameters.
+    """
+    sort_idx, Ps, num_harmonics, standing_wave = initialize_arrays(
+        num_harmonics, periods, standing_wave, custom_basis
+    )
     # Make the basis
-    t_values = np.arange(length) # Time stamps (row vector)
+    t_values = np.arange(length)  # Time stamps (row vector)
     B_fourier = []
     for ix, P in enumerate(Ps):
-        i_values = np.arange(1, num_harmonics[ix] + 1)[:, np.newaxis] # Harmonic indices (column vector)
+        i_values = np.arange(1, num_harmonics[ix] + 1)[
+            :, np.newaxis
+        ]  # Harmonic indices (column vector)
         if standing_wave[ix]:
-            w = 2 * np.pi / (P*2)
+            w = 2 * np.pi / (P * 2)
             B_sin = np.sin(i_values * w * np.mod(t_values, P))
             B_f = np.empty((length, num_harmonics[ix]), dtype=float)
             B_f[:] = B_sin.T
@@ -25,7 +64,6 @@ def make_basis_matrix(num_harmonics, length, periods, standing_wave=False, trend
             B_f[:, ::2] = B_cos.T
             B_f[:, 1::2] = B_sin.T
         B_fourier.append(B_f)
-    
     # Use custom basis if provided
     if custom_basis is not None:
         for ix, val in custom_basis.items():
@@ -39,7 +77,6 @@ def make_basis_matrix(num_harmonics, length, periods, standing_wave=False, trend
             # also reorder index of custom basis, if necessary
             ixt = np.where(sort_idx == ix)[0][0]
             B_fourier[ixt] = new_val
-
     # Add offset and linear terms
     if trend is False:
         B_P0 = np.ones((length, 1))
@@ -49,27 +86,58 @@ def make_basis_matrix(num_harmonics, length, periods, standing_wave=False, trend
         B_PL = np.linspace(-v, v, length).reshape(-1, 1)
         B_P0 = np.ones((length, 1))
         B0 = [B_PL, B_P0]
-
-    # Cross terms, this handles the case of no cross terms gracefully (empty list)
-    C = [cross_bases(*base_tuple, max_k=max_cross_k) for base_tuple in combinations(B_fourier, 2)]
-
+    # Cross terms, this handles the case of no cross terms gracefully
+    # (empty list)
+    C = [
+        cross_bases(*base_tuple, max_k=max_cross_k)
+        for base_tuple in combinations(B_fourier, 2)
+    ]
     B_list = B0 + B_fourier + C
     B = np.hstack(B_list)
     return B
 
-def make_regularization_matrix(num_harmonics, weight, periods, standing_wave=False, trend=False, max_cross_k=None, custom_basis=None):
-    
-    sort_idx, Ps, num_harmonics, standing_wave = initialize_arrays(num_harmonics, periods, standing_wave, custom_basis)
 
+def make_regularization_matrix(
+    num_harmonics,
+    weight,
+    periods,
+    standing_wave=False,
+    trend=False,
+    max_cross_k=None,
+    custom_basis=None,
+):
+    """
+    This function creates a regularization matrix for a time series with one or
+    multiple periods. It creates a regularization matrix for each period, then
+    combines them into a single matrix. The weights are determined using the
+    Dirichlet energy of the basis functions.
+
+    :param num_harmonics: int or 1darray of ints, number of harmonics to use for
+        each period.
+    :param weight: float, weight for the regularization matrix.
+    :param periods: float or 1darray of floats, periods of signal. Each period
+        will yield a bloc.
+    :param standing_wave: boolean or 1darray of booleans, whether to use standing
+        wave basis for each period.
+    :param trend: boolean, whether to include a linear trend in the basis matrix.
+        Defaults to False.
+    :param max_cross_k: int, maximum number of cross terms. Defaults to None.
+    :param custom_basis: dict, allows the user to pass a dictionary of custom basis
+        matrices.
+    :return: 2darray, regularization matrix. The shape depends on the parameters.
+    """
+    sort_idx, Ps, num_harmonics, standing_wave = initialize_arrays(
+        num_harmonics, periods, standing_wave, custom_basis
+    )
     ls_original = [weight * (2 * np.pi) / np.sqrt(P) for P in Ps]
-    # Create a sequence of values from 1 to K (repeated for cosine and sine when not standing wave)
+    # Create a sequence of values from 1 to K (repeated for cosine
+    # and sine when not standing wave)
     i_value_list = []
     for ix, nh in enumerate(num_harmonics):
         if standing_wave[ix]:
             i_value_list.append(np.arange(1, nh + 1))
         else:
             i_value_list.append(np.repeat(np.arange(1, nh + 1), 2))
-
     # Create blocks of coefficients
     blocks_original = [iv * lx for iv, lx in zip(i_value_list, ls_original)]
     if custom_basis is not None:
@@ -78,14 +146,12 @@ def make_regularization_matrix(num_harmonics, weight, periods, standing_wave=Fal
             blocks_original[ixt] = ls_original[ixt] * np.arange(1, val.shape[1] + 1)
     if max_cross_k is not None:
         max_cross_k *= 2
-    # this assumes  that the list of periods is ordered,  which is ensured in ln 51.  Ln 12 makes sure the bases are
-    # in the same order
-    blocks_cross = [[l2 for l1 in c[0][:max_cross_k] for l2 in c[1][:max_cross_k]]
-                    for c in combinations(blocks_original, 2)]
-    # This is *not* correct, as confirmed  with Stephen and Mehmet 8/31/23:
-    # blocks_cross = [[max(l1, l2) for l1 in c[0][:max_cross_k] for l2 in c[1][:max_cross_k]] for c in
-    #                 combinations(blocks_original, 2)]
-
+    # This assumes  that the list of periods is ordered, which is ensured
+    # when calling initialize_arrays.
+    blocks_cross = [
+        [l2 for l1 in c[0][:max_cross_k] for l2 in c[1][:max_cross_k]]
+        for c in combinations(blocks_original, 2)
+    ]
     # Combine the blocks to form the coefficient array
     if trend is False:
         first_block = [np.zeros(1)]
@@ -94,17 +160,79 @@ def make_regularization_matrix(num_harmonics, weight, periods, standing_wave=Fal
     coeff_i = np.concatenate(first_block + blocks_original + blocks_cross)
     # Create the diagonal matrix
     D = spdiags(coeff_i, 0, coeff_i.size, coeff_i.size)
-
     return D
 
+
+def initialize_arrays(num_harmonics, periods, standing_wave, custom_basis):
+    """
+    This function initializes the arrays for periods, harmonics, and standing wave.
+    It sorts everything in descending order of periods. It also performs some basic
+    checks on the input parameters.
+
+    :param num_harmonics: int or 1darray of ints, number of harmonics to use for
+        each period.
+    :param periods: float or 1darray of floats, periods of signal. Each period
+        will yield a bloc.
+    :param standing_wave: boolean or 1darray of booleans, whether to use standing wave
+        basis for each period.
+    :param custom_basis: dict, allows the user to pass a dictionary of custom basis
+        matrices.
+    :raises TypeError: raised if custom_basis is not a dictionary or None.
+    :raises ValueError: raised if num_harmonics and periods have different lengths.
+    :raises ValueError: raised if standing_wave and periods have different lengths.
+    :return: tuple, sorted indices, sorted periods, sorted number of harmonics, sorted
+        standing wave.
+    """
+    # Custom basis
+    if not (isinstance(custom_basis, dict) or custom_basis is None):
+        raise TypeError(
+            "custom_basis should be a dictionary where the key is the index\n"
+            + "of the period and the value is list containing the basis and the weights"
+        )
+    # Periods
+    Ps = np.atleast_1d(periods)
+    # Number of harmonics
+    num_harmonics = np.atleast_1d(num_harmonics)
+    if len(num_harmonics) == 1:
+        num_harmonics = np.tile(num_harmonics, len(Ps))
+    elif len(num_harmonics) != len(Ps):
+        raise ValueError(
+            "Please pass a single number of harmonics for all periods or a number\n"
+            + "for each period"
+        )
+    # Standing wave
+    standing_wave = np.atleast_1d(standing_wave)
+    if len(standing_wave) == 1:
+        standing_wave = np.tile(standing_wave, len(Ps))
+    elif len(standing_wave) != len(Ps):
+        raise ValueError(
+            "Please pass a single boolean for standing_wave for all periods or a\n"
+            + "boolean for each period"
+        )
+    # Sort the periods, harmonics, and standing wave
+    sort_idx = np.argsort(-Ps)
+    Ps = -np.sort(-Ps)  # Sort in descending order
+    num_harmonics = num_harmonics[sort_idx]
+    standing_wave = standing_wave[sort_idx]
+    return sort_idx, Ps, num_harmonics, standing_wave
+
+
 def cross_bases(B_P1, B_P2, max_k=None):
+    """
+    This function computes the cross terms between two basis matrices.
+
+    :param B_P1: 2darray, basis matrix for the first period.
+    :param B_P2: 2darray, basis matrix for the second period.
+    :param max_k: int, maximum number of cross terms. Defaults to None.
+    :return: 2darray, cross terms matrix.
+    """
     if max_k is None:
         # Reshape both arrays to introduce a new axis for broadcasting
         B_P1_new = B_P1[:, :, None]
         B_P2_new = B_P2[:, None, :]
     else:
-        B_P1_new = B_P1[:, :2*max_k, None]
-        B_P2_new = B_P2[:, None, :2*max_k]
+        B_P1_new = B_P1[:, : 2 * max_k, None]
+        B_P2_new = B_P2[:, None, : 2 * max_k]
     # Use broadcasting to compute the outer product for each row
     result = B_P1_new * B_P2_new
     # Reshape the result to the desired shape
@@ -113,39 +241,13 @@ def cross_bases(B_P1, B_P2, max_k=None):
 
 
 def pinball_slopes(quantiles):
+    """
+    This function computes the slopes for the pinball loss function.
+
+    :param quantiles: 1darray, quantiles.
+    :return: tuple, slopes.
+    """
     percentiles = np.asarray(quantiles)
-    a = (quantiles-.5)
-    b = (0.5)*np.ones((len(a),))
+    a = quantiles - 0.5
+    b = (0.5) * np.ones((len(a),))
     return a, b
-
-def initialize_arrays(num_harmonics, periods, standing_wave, custom_basis):
-
-    # Check custom_basis
-    if not (isinstance(custom_basis, dict) or custom_basis is None):
-        raise TypeError("custom_basis should be a dictionary where the key is the index\n" +
-                        "of the period and the value is list containing the basis and the weights")
-    
-    # Check periods
-    Ps = np.atleast_1d(periods)
-
-    # Check num_harmonics
-    num_harmonics = np.atleast_1d(num_harmonics)
-    if len(num_harmonics) == 1:
-        num_harmonics = np.tile(num_harmonics, len(Ps))
-    elif len(num_harmonics) != len(Ps):
-        raise ValueError("Please pass a single number of harmonics for all periods or a number for each period")
-    
-    # Check standing_wave
-    standing_wave = np.atleast_1d(standing_wave)
-    if len(standing_wave) == 1:
-        standing_wave = np.tile(standing_wave, len(Ps))
-    elif len(standing_wave) != len(Ps):
-        raise ValueError("Please pass a single boolean for standing_wave for all periods or a boolean for each period")
-    
-    # Sort the periods and harmonics
-    sort_idx = np.argsort(-Ps)
-    Ps = -np.sort(-Ps) # Sort in descending order
-    num_harmonics = num_harmonics[sort_idx]
-    standing_wave = standing_wave[sort_idx]
-
-    return sort_idx, Ps, num_harmonics, standing_wave
