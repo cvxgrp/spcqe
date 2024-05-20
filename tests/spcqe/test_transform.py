daa6677b0bf38ccd028844399be3fcc9a2f82c87
Aramis <116808832+AramisDuf@users.noreply.github.com>
2024-05-20 10:19:39 -0700

    modified tests

--- tests/spcqe/test_transform.py
+++ tests/spcqe/test_transform.py
@@ -31,46 +31,10 @@ class TestBasis(unittest.TestCase):
         transformed_b1 = spq1.transform(sig)
         np.testing.assert_allclose(transformed_a1, transformed_b1, rtol=1e-5, atol=1e-8)
 
-    # def test_transform2(self):
-    #     sig = np.load(os.path.join(self.directory, 'pvsig.npy'))
-    #     transformed_a2 = np.load(os.path.join(self.directory, 'transformed_data2.npy'))
-    #     spq2 = SmoothPeriodicQuantiles(
-    #         num_harmonics=[10,3],
-    #         periods=[nvals_dil, 365.24225*nvals_dil],
-    #         standing_wave=[True, False],
-    #         trend=False,
-    #         quantiles=my_quantiles,
-    #         weight=10,
-    #         problem='sequential',
-    #         solver='mosek',
-    #         verbose=False,
-    #     )
-    #     spq2.fit(sig)
-    #     transformed_b2 = spq2.transform(sig)
-    #     np.testing.assert_allclose(transformed_a2, transformed_b2, rtol=1e-5, atol=1e-8)
-
-    def test_transform3(self):
+    def test_transform2(self):
         sig = np.load(os.path.join(self.directory, 'pvsig.npy'))
-        transformed_a3 = np.load(os.path.join(self.directory, 'transformed_data3.npy'))
-        spq3 = SmoothPeriodicQuantiles(
-            num_harmonics=[10,3],
-            periods=[nvals_dil, 365.24225*nvals_dil],
-            standing_wave=[True, False],
-            trend=False,
-            quantiles=my_quantiles,
-            weight=10,
-            problem='full',
-            solver='mosek',
-            verbose=False,
-        )
-        spq3.fit(sig)
-        transformed_b3 = spq3.transform(sig)
-        np.testing.assert_allclose(transformed_a3, transformed_b3, rtol=1e-5, atol=1e-8)
-
-    def test_transform4(self):
-        sig = np.load(os.path.join(self.directory, 'pvsig.npy'))
-        transformed_a4 = np.load(os.path.join(self.directory, 'transformed_data4.npy'))
-        spq4 = SmoothPeriodicQuantiles(
+        transformed_a2 = np.load(os.path.join(self.directory, 'transformed_data2.npy'))
+        spq2 = SmoothPeriodicQuantiles(
             num_harmonics=[10,3],
             periods=[nvals_dil, 365.24225*nvals_dil],
             standing_wave=[True, False],
@@ -81,9 +45,9 @@ class TestBasis(unittest.TestCase):
             solver='clarabel',
             verbose=False,
         )
-        spq4.fit(sig)
-        transformed_b4 = spq4.transform(sig)
-        np.testing.assert_allclose(transformed_a4, transformed_b4, rtol=1e-5, atol=1e-8)
+        spq2.fit(sig)
+        transformed_b2 = spq2.transform(sig)
+        np.testing.assert_allclose(transformed_a2, transformed_b2, rtol=1e-5, atol=1e-8)
 
 if __name__ == "__main__":
     unittest.main()
